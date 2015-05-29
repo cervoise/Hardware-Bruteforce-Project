6b76e9c78599940b5f76cd43a53958d267253234
Julien R <julien@reitzel.info>
2015-05-29 16:09:00 +0200

    Add arduino code to be able to test the library

--- tests/boards/Generic/_005-LineByLine/test_LineByLine/test_LineByLine.ino
+++ tests/boards/Generic/_005-LineByLine/test_LineByLine/test_LineByLine.ino
@@ -1,23 +1,42 @@
+/*
+
+* For reading files, SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+*/
+
+// CS pin for SD card
+const int sdCsPin = 4;
+
+// Set value to true if wordlist actually is a list of files
+#define READ_LINE_BY_LINE_IN_FILES false
 
 #include <SPI.h>
 #include <SD.h>
 
-char *login[] = {""};
 
-#include <LineByLine.h>
-LineByLine lineByLine(login);
-
-/*
-#include <LineByLineReadFiles.h>
-LineByLineReadFiles lineByLine(login, 10);
-*/
+#ifdef READ_LINE_BY_LINE_IN_FILES
+  #include <LineByLineReadFiles.h>
+  char *wordfiles[] = {"file1.txt", "file2.txt"};
+  LineByLineReadFiles lineByLine(wordfiles, sdCsPin);
+#else
+  #include <LineByLine.h>
+  char *wordlist[] = {"lorem", "ipsum", "dolor", "sit", "amet"};
+  LineByLine lineByLine(wordlist);
+#endif
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while(lineByLine.hasNext()) {
+    Serial.println(lineByLine.next());
+  }
 }
