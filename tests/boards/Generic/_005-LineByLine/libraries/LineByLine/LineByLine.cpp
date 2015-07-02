4c0d2f1f8c1c2ea41f52a866786f11448f190500
Julien Reitzel <julien@reitzel.info>
2015-07-02 15:23:11 +0200

    Initialize char* returned by LineByLine::next() with a malloc, so we can use free like for LineByLineReadFiles::next()

--- tests/boards/Generic/_005-LineByLine/libraries/LineByLine/LineByLine.cpp
+++ tests/boards/Generic/_005-LineByLine/libraries/LineByLine/LineByLine.cpp
@@ -22,8 +22,15 @@ char* LineByLine::next()
 	if (!hasNext())
 	{
 		//TODO: throw an error
-	} 
-	return dataList.array[pos++];	
+	}
+	
+	char *line = dataList.array[pos++];
+	
+	// Initialize a char* with malloc, because the user expects to do a free() with the char returned by next()
+	char *res = (char*) malloc((strlen(line) + 1) * sizeof(char));
+	strcpy(line, res);
+	
+	return res;	
 }
 
 
